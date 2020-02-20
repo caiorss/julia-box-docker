32f8e159201e3266560b98dc64be3bd884a539bf
CaioRS <caiorss.rodrigues@gmail.com>
2020-02-20 00:14:53 -0300

    Modify wrapper script for running ptipython.

--- julia-box-shell.sh
+++ julia-box-shell.sh
@@ -97,7 +97,7 @@ EOF
        -w /work \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -v $HOME/.Xauthority:/root/.Xauthority \
-       --net=host $DOCKER_IMAGE_NAME $PYTHON $CONDA_BIN/ipython 
+       --net=host $DOCKER_IMAGE_NAME $PYTHON $CONDA_BIN/ptipython -i /home/eniac/ipython_profile.py
 
         xhost -
         ;;
